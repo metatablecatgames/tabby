e0f6701e7d563d10ded1043527a13393179cb525
tfilteredc <132796135+tfilteredc@users.noreply.github.com>
2023-11-13 15:50:50 +0000

    Add default project

---
+++ src/Project.lua
@@ -0,0 +1,18 @@
+-- This is an empty shell for now, but provides docs on what each key does
+
+return {
+	Name = "Tabby",
+
+	Fragments = {
+		-- define fragment names here by linking them to module scripts
+		-- main = script.Parent.Fragments.main
+	},
+
+	FragmentGroups = {
+		-- define groups that could be ran, uses fragment names defined above
+		-- main = {"main"}
+	},
+
+	-- point to a group that will be ran
+	ActiveGroup = "main"
+}
